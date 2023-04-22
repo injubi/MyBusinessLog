a712c930e13f34e7ecd3e29d97d242c45d0196de
thetruthrunswild <thetruthrunswild@naver.com>
2023-04-22 14:04:04 +0900

    targetDay update

--- src/store/modules/dayHooks.ts
+++ src/store/modules/dayHooks.ts
@@ -4,28 +4,43 @@ import { RootState } from ".";
 import { targetDayAction } from "./day";
 
 const useDay = () => {
-  const { today, targetDay } = useSelector((state: RootState) => state.day);
-  const dispatch = useDispatch();
+    const { today, targetDay } = useSelector((state: RootState) => state.day);
+    const dispatch = useDispatch();
 
-  const handleTargetDay = useCallback((option: string) => {
-    let nowMonth = targetDay.getMonth();
-    let newDate = new Date();
-    if (option === "prev") {
-      newDate = new Date(
-        targetDay.getFullYear(),
-        nowMonth - 1,
-        newDate.getDate()
-      );
-    } else {
-      newDate = new Date(
-        targetDay.getFullYear(),
-        nowMonth + 1,
-        newDate.getDate()
-      );
-    }
-    console.log(newDate);
-    dispatch(targetDayAction({ targetDay: newDate }));
-  }, []);
-  return { today, targetDay, handleTargetDay };
+    const handleTargetDay = useCallback(
+        (option: string) => {
+            let newDate = new Date();
+
+            if (option === "prev") {
+                if (targetDay.getMonth() === 1) {
+                    newDate = new Date(
+                        targetDay.getFullYear() - 1,
+                        11,
+                        newDate.getDate()
+                    );
+                } else {
+                    newDate = new Date(
+                        targetDay.getFullYear(),
+                        targetDay.getMonth() - 1,
+                        newDate.getDate()
+                    );
+                }
+            } else {
+                // newDate = new Date(targetDay.getFullYear(), nowMonth + 1);
+                if (targetDay.getMonth() === 11) {
+                    newDate = new Date(targetDay.getFullYear() + 1, 0, 1);
+                } else {
+                    newDate = new Date(
+                        targetDay.getFullYear(),
+                        targetDay.getMonth() + 1,
+                        newDate.getDate()
+                    );
+                }
+            }
+            dispatch(targetDayAction({ targetDay: newDate }));
+        },
+        [targetDay]
+    );
+    return { today, targetDay, handleTargetDay };
 };
 export default useDay;
