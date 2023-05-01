400cb4d18a2ed582aa80e4f227d002f315432029
thetruthrunswild <thetruthrunswild@naver.com>
2023-05-01 20:33:01 +0900

    error

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -4,46 +4,47 @@ import { useEffect, useState } from "react";
 import useLog from "../store/modules/log/logHooks";
 
 export const DayContainer = styled.div<{ isToday: boolean }>`
-  height: 100px;
-  background-color: white;
-  padding: 10px;
-  font-weight: ${(props) => props.isToday && 600};
-  /* border: ${(props) => props.isToday && "1px solid yellow"}; */
-  box-shadow: ${(props) => props.isToday && "0 0 0 1px yellow"};
-  border-radius: 10px;
-  color: gray;
-  display: flex;
-  justify-content: space-between;
+    height: 100px;
+    background-color: white;
+    padding: 10px;
+    font-weight: ${(props) => props.isToday && 600};
+    /* border: ${(props) => props.isToday && "1px solid yellow"}; */
+    box-shadow: ${(props) => props.isToday && "0 0 0 1px yellow"};
+    border-radius: 10px;
+    color: gray;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const Check = styled.div`
-  /* margin-left: 10px; */
+    /* margin-left: 10px; */
 `;
 
 export type DayProps = {
-  day: number;
-  isToday: boolean;
-  dateObject: Date;
+    day: number;
+    isToday: boolean;
+    dateObject: Date;
 };
 
 const Day = ({ day, isToday, dateObject }: DayProps) => {
-  const { targetDay, clickDay, checkSameDate } = useDay();
-  const { data } = useLog();
-  const [hasContents, setHasContents] = useState<boolean>(false);
-  useEffect(() => {
-    data.forEach((d) => {
-      if (checkSameDate(d.date, dateObject)) {
-        console.log("here");
-        setHasContents(true);
-      }
-    });
-  }, [data]);
-  return (
-    <DayContainer isToday={isToday} onClick={() => clickDay(dateObject)}>
-      <div>{day}</div>
-      {hasContents && <Check>●</Check>}
-    </DayContainer>
-  );
+    const { clickDay, checkSameDate } = useDay();
+    const { data } = useLog();
+    const [hasContents, setHasContents] = useState<boolean>(false);
+    useEffect(() => {
+        setHasContents(false);
+        data.forEach((d) => {
+            if (checkSameDate(d.date, dateObject)) {
+                setHasContents(true);
+            } else {
+            }
+        });
+    }, [day, dateObject, checkSameDate, data]);
+    return (
+        <DayContainer isToday={isToday} onClick={() => clickDay(dateObject)}>
+            <div>{day}</div>
+            {hasContents && <Check>●</Check>}
+        </DayContainer>
+    );
 };
 
 export default Day;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,45 +4,47 @@ import useLog from "../store/modules/log/logHooks";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  padding: 20px 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 20px 0;
 `;
 const TextArea = styled.textarea`
-  width: 70%;
-  height: 30px;
-  border: 1px solid lightgray;
-  border-radius: 10px;
+    width: 70%;
+    height: 30px;
+    border: 1px solid lightgray;
+    border-radius: 10px;
 `;
 
 const Button = styled.button`
-  border: 0;
-  background-color: #a4d6db;
-  color: black;
-  width: 100px;
-  margin: 0 10px;
-  border-radius: 20px;
-  cursor: pointer;
+    border: 0;
+    background-color: #a4d6db;
+    color: black;
+    width: 100px;
+    margin: 0 10px;
+    border-radius: 20px;
+    cursor: pointer;
 `;
 const Input = () => {
-  const [content, setContent] = useState<string>();
-  const { targetDay } = useDay();
-  const { insertLog } = useLog();
+    const [content, setContent] = useState<string>();
+    const { targetDay } = useDay();
+    const { insertLog } = useLog();
 
-  const handleClickBtn = () => {
-    console.log(content);
-    if (content) {
-      insertLog({ content: content, date: targetDay });
-      setContent("");
-    }
-  };
-  return (
-    <Container>
-      <TextArea value={content} onChange={(e) => setContent(e.target.value)} />
-      <Button onClick={handleClickBtn}>등록</Button>
-    </Container>
-  );
+    const handleClickBtn = () => {
+        if (content) {
+            insertLog({ content: content, date: targetDay });
+            setContent("");
+        }
+    };
+    return (
+        <Container>
+            <TextArea
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+            />
+            <Button onClick={handleClickBtn}>등록</Button>
+        </Container>
+    );
 };
 
 export default Input;
